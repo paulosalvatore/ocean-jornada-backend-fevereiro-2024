6b9cea6bdb2985570f6b562680a80210f3cb706c
Paulo Salvatore <salvatoregames@gmail.com>
2024-02-21 14:23:24 +0000

    Endpoint `[GET] /item` criado

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello, World!')
 })
 
 app.get('/oi', function (req, res) {
-    res.send('Olá, mundo!')
+  res.send('Olá, mundo!')
+})
+
+// Lista de Personagens
+const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+
+// Read All -> [GET] /item
+app.get('/item', function (req, res) {
+  res.send(lista)
 })
 
 app.listen(3000)
