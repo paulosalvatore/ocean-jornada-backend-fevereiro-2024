bcddc9b7cf4942c2b97482a3e17ac7bdbaecbebe
Paulo Salvatore <salvatoregames@gmail.com>
2024-02-21 14:38:34 +0000

    Endpoint `[GET] /item/:id` criado

--- index.js
+++ index.js
@@ -11,10 +11,24 @@ app.get('/oi', function (req, res) {
 
 // Lista de Personagens
 const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+//              0               1              2
 
 // Read All -> [GET] /item
 app.get('/item', function (req, res) {
+  // Envio a lista inteira como resposta HTTP
   res.send(lista)
+})
+
+// Read By ID -> [GET] /item/:id
+app.get('/item/:id', function (req, res) {
+  // Acesso o ID no parâmetro de rota
+  const id = req.params.id
+
+  // Acesso item na lista baseado no ID recebido
+  const item = lista[id]
+
+  // Envio o item obtido como resposta HTTP
+  res.send(item)
 })
 
 app.listen(3000)
